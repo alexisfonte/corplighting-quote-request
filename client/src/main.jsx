a571e62716f05c3337452be761cc05fb913bcf9a
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-03-22 22:13:05 -0500

    succesful login and session creation on front end

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 )
